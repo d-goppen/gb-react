320e48a3360acebbd11df255cc1aaee079e00169
D.Goppen <51300326+d-goppen@users.noreply.github.com>
2022-04-11 03:46:33 +0300

    Компонент для несуществующих страниц.

---
+++ src/pages/Page404.js
@@ -0,0 +1,12 @@
+import { Alert } from "@mui/material";
+
+
+const Page404 = () => {
+  return (
+    <Alert variant="outlined" severity="error">
+      404: Page not found, try another.
+    </Alert>
+  );
+};
+
+export default Page404;
